4e2c285d9d01c9083d64777adbd38491e4638639
Prathuvj <vijayvargiyaprathu@gmail.com>
2025-07-08 18:34:04 +0530

    Initial Commit

---
+++ metadata.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+from moviepy.editor import VideoFileClip
+
+def extract_video_metadata(path):
+    clip = VideoFileClip(path)
+    cap = cv2.VideoCapture(path)
+    size = os.path.getsize(path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap.release()
+    return {
+        'Duration (s)': round(clip.duration, 2),
+        'Resolution': f'{width}x{height}',
+        'File Size (MB)': round(size / (1024 * 1024), 2),
+        'Frame Rate (fps)': round(fps, 2),
+        'File Format': os.path.splitext(path)[1].replace('.', '')
+    }
